f454b4f398412e716d2c7d0b82aa4654d11d2c62
PiotrSroka2005 <piotr09sroka@interia.pl>
2024-10-23 08:38:47 +0200

    Add AlbumData structure

---
+++ konsolowa/AlbumData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konsolowa
+{
+    public struct AlbumData
+    {
+        public string artist {  get; set; }
+
+        public string album { get; set; }
+
+        public int songsNumber { get; set; }
+
+        public int year { get; set; }
+
+        public int dowlandNumber { get; set; }
+    }
+}
--- konsolowa/Program.cs
+++ konsolowa/Program.cs
@@ -1,10 +1,10 @@
 ﻿namespace konsolowa
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
     }
 }
